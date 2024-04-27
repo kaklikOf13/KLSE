eba717a2787339ea04accfa1942a865e68949503
kaklikOf13 <101949387+kaklikOf13@users.noreply.github.com>
2024-04-27 16:58:31 -0300

    Change main.ts
    change the klse url

--- executable/templates/IO/server/src/main.ts
+++ executable/templates/IO/server/src/main.ts
@@ -1,6 +1,6 @@
 import { dirname } from"https://deno.land/std/path/mod.ts"
 import { GameServerConfig,GameServer } from "./server.ts"
-import { Server } from "https://deno.land/x/KLSE/server_side/server.ts"
+import { Server } from "https://deno.land/x/klse/server_side/server.ts"
 
 //Init
 if(import.meta.dirname&&Deno.cwd()!=dirname(import.meta.dirname)){
