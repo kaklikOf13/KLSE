3705e71522c4d59d375dcc66332039c41e96cad5
kaklikOf13 <101949387+kaklikOf13@users.noreply.github.com>
2024-04-27 16:57:48 -0300

    Change server.ts

--- executable/templates/IO/server/src/server.ts
+++ executable/templates/IO/server/src/server.ts
@@ -1,4 +1,4 @@
-import { Server } from "https://deno.land/x/KLSE/server_side/server.ts"
+import { Server } from "https://deno.land/x/klse/server_side/server.ts"
 import {GameConfig} from "./game.ts"
 export interface GameServerConfig{
     config:GameConfig,
