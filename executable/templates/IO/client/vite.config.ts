58fca45a7af6828ee40f797b325c9adf837a82b3
kaklikOf13 <canaldokaklik@gmail.com>
2024-06-17 08:35:00 -0300

    refactor:update_version

--- executable/templates/IO/client/vite.config.ts
+++ executable/templates/IO/client/vite.config.ts
@@ -8,11 +8,11 @@ export default {
     alias: {
       "common":path.resolve(__dirname,"../common"),
 
-      /*"KLSE/CLIENT":"https://deno.land/x/klse@0.1.8/client_side/bundle.js",
-      "KLSE":"https://deno.land/x/klse@0.1.8/bundle.js"*/
+      "KLSE/CLIENT":"https://deno.land/x/klse@0.2/client_side/bundle.js",
+      "KLSE":"https://deno.land/x/klse@0.2/bundle.js"
 
-      "KLSE/CLIENT":path.resolve(__dirname,"../../../../client_side/bundle.js"),
-      "KLSE":path.resolve(__dirname,"../../../../bundle.js"),
+      /*"KLSE/CLIENT":path.resolve(__dirname,"../../../../client_side/bundle.js"),
+      "KLSE":path.resolve(__dirname,"../../../../bundle.js"),*/
     },
   },
   server:{
