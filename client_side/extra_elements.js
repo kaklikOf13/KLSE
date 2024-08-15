b18c4cde9bf1f44bb87e183403d3a2afe5c0ca2e
kaklikOf13 <canaldokaklik@gmail.com>
2024-08-14 21:33:29 -0300

    feat:menu_element

--- client_side/extra_elements.js
+++ client_side/extra_elements.js
@@ -42,5 +42,79 @@ class TabsContainer extends HTMLElement{
         });
     }
 }
-
-customElements.define('tabs-container', TabsContainer);
+class Menu extends HTMLElement {
+    constructor(){
+        super()
+    }
+    connectedCallback(){
+        const eventoMouseEnter = new Event('mouseenter')
+        this.addEventListener("mouseenter",()=>{
+            if(this.parentElement!=null&&this.parentElement.tagName=="kl-submenu"){
+                this.parentElement.style.display="block"
+                this.style.display="block"
+                this.parentElement.parentElement.dispatchEvent(eventoMouseEnter)
+            }
+        })
+        this.addEventListener("mouseleave",()=>{
+            if(this.parentElement!=null&&this.parentElement.tagName=="kl-submenu"){
+                this.style.display="none"
+                setTimeout(() => {
+                    if(!this.parentElement.parentElement.mouse_inside){
+                        this.parentElement.style.display="none"
+                        this.style.display="none"
+                    }
+                }, 10)
+            }
+        })
+        this.addEventListener("click",()=>{
+            setTimeout(()=>{
+                this.remove()
+            },100)
+        })
+    }
+    /**
+     * 
+     * @param {string} text 
+     * @param {(event:MouseEvent)=>void} onclick 
+     */
+    add_option(text,onclick=(_e)=>{}){
+        const node=document.createElement("kl-option")
+        node.innerText=text
+        node.addEventListener("click",onclick)
+        this.appendChild(node)
+    }
+    /**
+     * 
+     * @param {string} text
+     * @param {Menu} menu
+     * @param {(event:MouseEvent)=>void} onclick
+     */
+    add_submenu(text,menu,onclick=(_e)=>{}){
+        const node=document.createElement("kl-submenu")
+        node.innerText=text
+        node.addEventListener("click",onclick)
+        node.appendChild(menu)
+        this.appendChild(node)
+    }
+}
+class SubMenu extends HTMLElement{
+    constructor(){
+        super()
+        this.mouse_inside=false
+        this.menu=null
+    }
+    connectedCallback(){
+        this.addEventListener("mouseenter",()=>{
+            this.menu=this.querySelector("kl-menu")
+            this.mouse_inside=true
+            this.menu.style.display="block"
+        })
+        this.addEventListener("mouseleave",()=>{
+            this.mouse_inside=false
+            this.menu.style.display="none"
+        })
+    }
+}
+customElements.define('tabs-container', TabsContainer)
+customElements.define("kl-menu", Menu)
+customElements.define("kl-submenu", SubMenu)
